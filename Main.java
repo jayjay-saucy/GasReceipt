20d3c345ef5e77e2f5f6686f7ba85fec4a4e90ea
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-03-02 13:55:11 -0500

    Gas receipt commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("______________________________________");
+	System.out.println("|         Jay Jay's Pump N' Go       |");
+	System.out.println("|           17842 Main St            |");
+	System.out.println("|              Newton                |");
+	System.out.println("|           Carolina 93842           |");
+	System.out.println("|                                    |");
+	System.out.println("|  ST-TX:  11089984   (555) 708-2224 |");
+	System.out.println("|  2020-01-13 IW424534:9338300 01:52 |");
+	System.out.println("|                                    |");
+	System.out.println("|  Fuel         (G)   ($/G)      ($) |");
+	System.out.println("|  Pump 5                            |");
+	System.out.println("|  Premium     40.000  1.345  53.80* |");
+	System.out.println("|                                    |");
+	System.out.println("|  Total Owed                 53.80  |");
+	System.out.println("|  Total Paid                        |");
+	System.out.println("|  Credit Card                53.80  |");
+	System.out.println("|                                    |");
+	System.out.println("|  VISA       ********4597           |");
+	System.out.println("|  INV.  972821 Auth.   545633       |");
+	System.out.println("|  Purchase                          |");
+	System.out.println("|  S 0010010010 00 127               |");
+	System.out.println("|  00 APPROVED - THANK YOU           |");
+	System.out.println("|                                    |");
+	System.out.println("|         --IMPORTANT--              |");
+	System.out.println("|  Retain This Copy For Your Records |");
+	System.out.println("______________________________________");
+
+    }
+}
